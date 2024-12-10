23db6d93f1d5051e065d9c5a59af1a5a11509e2a
Josh Panebianco <joshpanebianco@protonmail.com>
2024-12-10 23:58:27 +1000

    styling change to connect wallet button

--- frontend/src/components/ConnectWallet.jsx
+++ frontend/src/components/ConnectWallet.jsx
@@ -73,7 +73,7 @@ const ConnectWallet = ({ setIsConnected }) => {
       {/* Connect Wallet Button */}
       <button
         onClick={connectWalletMetamask}
-        className="w-80 px-8 py-4 bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500 text-white font-semibold text-xl rounded-lg shadow-md hover:bg-purple-400 focus:outline-none focus:ring-4 focus:ring-purple-300 transition-all"
+        className="w-80 px-8 py-4 bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500 text-white font-semibold text-xl rounded-lg shadow-md hover:from-purple-600 hover:via-blue-500 hover:to-purple-600 focus:outline-none focus:ring-4 focus:ring-purple-300 transition-all"
       >
         {address
           ? `Connected: ${address.substring(0, 6)}...${address.substring(
