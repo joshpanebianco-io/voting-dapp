dddf1b38f6bed54c6a0277db909b0708c2244b51
Josh Panebianco <joshpanebianco@protonmail.com>
2025-06-05 01:13:04 +1000

    fixed poll details url

--- frontend/src/components/ActivePoll.jsx
+++ frontend/src/components/ActivePoll.jsx
@@ -291,7 +291,7 @@ const ActivePoll = ({ isConnected, refreshKey, onPollClose }) => {
 
   const handlePollClick = (pollId) => {
     setSelectedPoll(pollId);
-    navigate(`active?pollId=${pollId}`);
+    navigate(`/voting-dapp/active?pollId=${pollId}`);
   };
 
   const closeSuccessModal = () => {
